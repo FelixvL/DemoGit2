8a4dc10afb8c02ed5f0e070d8b055b81a6e1632b
FelixvL <digitaleservice@gmail.com>
2017-01-19 15:16:40 +0100

    Gelukkig heb ik mijn oude status weer

--- src/package1/DemoGit.java
+++ src/package1/DemoGit.java
@@ -3,8 +3,6 @@ package package1;
 public class DemoGit {
 	public static void main(String[] args){
 		System.out.println("Here we go");
-		System.out.println("Hier is mijn tweede stap");
-		System.out,println("Wat is er aan de hand");
 	}
 
 }
